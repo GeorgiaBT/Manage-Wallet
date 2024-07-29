b4c68d1bc69a9b9713060edb2e7ad15641f67df6
João Silva <gabrielvsc@id.uff.br>
2024-07-29 17:32:12 -0300

    code update

--- project_wallet.py
+++ project_wallet.py
@@ -1,3 +1,5 @@
+import time
+import datetime
 # Menu de funções
 
 saldo = 0.0
@@ -17,6 +19,7 @@ saude = 0.0
 vestuario = 0.0
 outro = 0.0
 
+
 while True:
     print("Qual operação deseja realizar?")
     print("1️⃣ - Ver Saldo")
@@ -24,6 +27,7 @@ while True:
     print("3️⃣ - Cadastrar saída de valores")
     print("4️⃣ - Cadastrar meta de economia")
     print("5️⃣ - Relatórios")
+    print("6️⃣ - Sair")
 
     print("-------------------------------------")
 
@@ -33,6 +37,8 @@ while True:
         print("-------------------------------------")
         print(f"Saldo: R${saldo:.2f}")
         print("-------------------------------------")
+        time.sleep(1)
+
     
     elif opcao == 2:
         print("-------------------------------------")
@@ -43,6 +49,7 @@ while True:
 
             if entrada < 0:
                 print("Valor inválido! Tente novamente.")
+                time.sleep(1)
                 continue
             else:
                 break
@@ -51,61 +58,67 @@ while True:
         print("-------------------------------------")
     
     elif opcao == 3:
-        print("-------------------------------------")
-        print("Insira o valor gasto: ")
-
         while True:
+            print("-------------------------------------")
+            print("Insira o valor gasto: ")
+            print("Digite '0' para sair: ")
+
             saida = float(input())
 
             if saida < 0:
                 print("Valor inválido! Tente novamente.")
+                time.sleep(1)
                 continue
-            else:
-                break
-            
-        while True: 
-            print("Onde esse dinheiro foi gasto?")
-            print("1️⃣ - Alimentação")
-            print("2️⃣ - Transporte")
-            print("3️⃣ - Moradia")
-            print("4️⃣ - Saúde")
-            print("5️⃣ - Educação")
-            print("6️⃣ - Lazer")
-            print("7️⃣ - Vestuário")
-            print("8️⃣ - Outro")
-            opt = int(input())
-
-            if opt == 1:
-                alimentacao += saida
-                break
-            elif opt == 2:
-                transporte += saida
-                break
-            elif opt == 3:
-                moradia += saida
-                break
-            elif opt == 4:
-                saude += saida
-                break
-            elif opt == 5:
-                educacao += saida
-                break
-            elif opt == 6:
-                lazer += saida
-                break
-            elif opt == 7:
-                vestuario += saida
-                break
-            elif opt == 8:
-                outro += saida
+            elif saida == 0:
                 break
             else:
-                print("Opção inválida! Tente novamente.")
-                continue
+                        
+                while True: 
+                    print("Onde esse dinheiro foi gasto?")
+                    print("1️⃣ - Alimentação")
+                    print("2️⃣ - Transporte")
+                    print("3️⃣ - Moradia")
+                    print("4️⃣ - Saúde")
+                    print("5️⃣ - Educação")
+                    print("6️⃣ - Lazer")
+                    print("7️⃣ - Vestuário")
+                    print("8️⃣ - Outro")
+                    opt = int(input())
 
-        saldo -= saida
-        despesa_total += saida
-        print("-------------------------------------")
+                    if opt == 1:
+                        alimentacao += saida
+                        break
+                    elif opt == 2:
+                        transporte += saida
+                        break
+                    elif opt == 3:
+                        moradia += saida
+                        break
+                    elif opt == 4:
+                        saude += saida
+                        break
+                    elif opt == 5:
+                        educacao += saida
+                        break
+                    elif opt == 6:
+                        lazer += saida
+                        break
+                    elif opt == 7:
+                        vestuario += saida
+                        break
+                    elif opt == 8:
+                        outro += saida
+                        break
+                    else:
+                        print("Opção inválida! Tente novamente.")
+                        time.sleep(1)
+                        continue
+
+                saldo -= saida
+                despesa_total += saida
+                print("-------------------------------------")
+
+
 
     elif opcao == 4:
         print("-------------------------------------")
@@ -116,10 +129,14 @@ while True:
 
             if economia < 0:
                 print("Valor inválido! Tente novamente.")
+                time.sleep(1)
                 continue
+
             else:
                 break
         print("-------------------------------------")
+
+
 
     elif opcao == 5:
         print("-------------------------------------")
@@ -129,37 +146,105 @@ while True:
             print("2️⃣ - Saída de valores")
             print("3️⃣ - Despesas por categoria")
             print("4️⃣ - Meta de economia")
+            print("5️⃣ - Imprimir relatório total")
             opt = int(input())
 
             if opt == 1:
                 print(f"Entrada de valores: R${entrada:.2f}")
+                time.sleep(1)
                 break
+
+
             elif opt == 2:
                 print(f"Saída de valores: R${despesa_total:.2f}")
+                time.sleep(1)
                 break
+
+
             elif opt == 3:
-                print("--- DESPESAS POR CATEGORIA ---")
-                print(f"Alimentação: R${alimentacao:.2f}")
-                print(f"Moradia: R${moradia:.2f}")
-                print(f"Transporte: R${transporte:.2f}")
-                print(f"Saúde: R${saude:.2f}")
-                print(f"Educação: R${educacao:.2f}")
-                print(f"Lazer: R${lazer:.2f}")
-                print(f"Vestuário: R${vestuario:.2f}")
-                print(f"Outro: R${outro:.2f}")
+                print("📊 --- DESPESAS POR CATEGORIA --- 📊")
+                time.sleep(0.5)
+                print(f"🍽️ Alimentação: R${alimentacao:.2f}")
+                time.sleep(0.5)
+                print(f"🏠 Moradia: R${moradia:.2f}")
+                time.sleep(0.5)
+                print(f"🚗 Transporte: R${transporte:.2f}")
+                time.sleep(0.5)
+                print(f"🩺 Saúde: R${saude:.2f}")
+                time.sleep(0.5)
+                print(f"📚 Educação: R${educacao:.2f}")
+                time.sleep(0.5)
+                print(f"🎉 Lazer: R${lazer:.2f}")
+                time.sleep(0.5)
+                print(f"👗 Vestuário: R${vestuario:.2f}")
+                time.sleep(0.5)
+                print(f"📦 Outro: R${outro:.2f}")
+                time.sleep(0.5)
                 break
+
+
             elif opt == 4:
+                print(f"🎯 Saldo Atual: R${saldo:.2f}")
+                time.sleep(0.5)
                 print(f"Meta de economia: R${economia:.2f}")
                 if saldo >= economia:
                     print("Status: ATINGIDA!")
+                    time.sleep(1)
                 else:
                     print("Status: PENDENTE")
                     print(f"Faltam R${economia - saldo:.2f} para atingir a meta!")
+                    time.sleep(1)
                 break
+
+            #opcao nova (imprimir relatório total)
+            elif opt == 5:
+                print("💰💼 Relatório Financeiro 💼💰")
+                time.sleep(0.5)
+                print(f"📥 Entrada de valores: R${entrada:.2f}")
+                print()
+                time.sleep(0.5)
+                print(f"📤 Saída de valores: R${despesa_total:.2f}")
+                print()
+                time.sleep(0.5)
+                print("📊 --- DESPESAS POR CATEGORIA --- 📊")
+                time.sleep(0.5)
+                print(f"🍽️ Alimentação: R${alimentacao:.2f}")
+                time.sleep(0.5)
+                print(f"🏠 Moradia: R${moradia:.2f}")
+                time.sleep(0.5)
+                print(f"🚗 Transporte: R${transporte:.2f}")
+                time.sleep(0.5)
+                print(f"🩺 Saúde: R${saude:.2f}")
+                time.sleep(0.5)
+                print(f"📚 Educação: R${educacao:.2f}")
+                time.sleep(0.5)
+                print(f"🎉 Lazer: R${lazer:.2f}")
+                time.sleep(0.5)
+                print(f"👗 Vestuário: R${vestuario:.2f}")
+                time.sleep(0.5)
+                print(f"📦 Outro: R${outro:.2f}")
+                print()
+                time.sleep(0.5)
+                print(f"🎯 Meta de economia: R${economia:.2f}")
+                time.sleep(0.5)
+                print(f"🎯 Saldo Atual: R${saldo:.2f}")
+                time.sleep(0.5)
+                if saldo >= economia:
+                    print("🎉 Status: ATINGIDA! 🎉")
+                else:
+                    print("⚠️ Status: PENDENTE ⚠️")
+                    print(f"Faltam R${economia - saldo:.2f} para atingir a meta!")
+                time.sleep(1)
+                break
+
+
+
             else:
                 print("Opção inválida! Tente novamente.")
                 continue
         print("-------------------------------------")
+    elif opcao == 6:
+        break
 
     else:
         print("Opção inválida! Tente novamente.")
